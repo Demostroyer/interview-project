5210f4c3962340cfb8fbebbb6cd0df953e8de5f3
Demostroyer <dempseyin@live.co.uk>
2017-09-21 08:56:41 +0100

    Updating comments for better understanding

--- src/mu_ian_dempsey/twoSum.java
+++ src/mu_ian_dempsey/twoSum.java
@@ -4,14 +4,14 @@ import java.util.Arrays;
 /*
  * This is the twoSum problem on leetCode.
  * I have planned out the following approach:
- * given an array and a sum target, find a pair of digits whihc will equal that sum
- * ie [1,2,3,4] sum = 6. We can assume they are in ascending order
+ * given an array and a sum target, find a pair of digits which will equal that sum
+ * ie [1,2,3,4] sum = 6. We can't assume they are in order, so must take that into account.
  * so can use two for loops, but is really slow, O(N^2)
- * can maybe take one element, say 2, then we know we need a four to get to 6, so we seach for a 4. 
+ * can maybe take one element, say 2, then we know we need a four to get to 6, so we search for a 4. 
  *  this is a binary search and is O(log)
- * best would be using bounds. so take most extrme as a pair, if they are bigger, then move in the 
- * right upperbound, if it is left, then move in the left upperbound. 
- * stop codnition:if we find a pair equaling our target, or if they cross = not found
+ * best would be using bounds. so take most extreme as a pair, if total is bigger, then move in the 
+ * right upperbound, if it is less, then move in the left upperbound. 
+ * stop cond:if we find a pair equalling our target, or if they cross = not found
  *  NOTE: i have taken into account negative numbers, unsorted arrays(which i sort to solve the Q)
  * @author Ian Dempsey
  * @date 19/09/2017
@@ -57,14 +57,18 @@ public class twoSum {
 		}
 		//now just return the index of the unsorted array, this will not affect input which is sorted. 
         int count=0;
+        //using the copy of original arry which is unsorted
         for(int i =0;i <num2.length;i++){
+        	//if the unsorted array's value at index i = our sorted arrays value at lowerBound index
+        	//& count==0, (this just stops the index being overwrritten, applies to unsorted arrays)
            if(num2[i]==nums[lowerBound] && count==0) {
        //     System.out.println("num2[0]" + num2[0]);
-               solution[0]=i;
+               solution[0]=i;//first index for solution pair
                count++;
             }
+           //in the original unsorted array, if the array's val at index i= sorted array's val at upper
             else if(num2[i]==nums[upperBound]){
-                solution[1]=i;
+                solution[1]=i;//second index for soultion pair
             }
         }
     return solution;
