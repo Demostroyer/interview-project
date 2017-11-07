0dcaba2b6895b856b1f0fee3c567ba2b5b6ff801
Demostroyer <dempseyin@live.co.uk>
2017-11-07 22:28:28 +0000

    Intro paragraph for The Solution is nearly done

--- src/mu_ian_dempsey/leetcode23.java
+++ src/mu_ian_dempsey/leetcode23.java
@@ -16,6 +16,7 @@ package mu_ian_dempsey;
 	Then continue like normal for mergeSort! 
 	@author Ian Dempsey
 	@date 10/10/2017	
+	Pattern:Merge Sort
 	Time Analysis: O(nlogn) -> Using mergesort so it is going to be this complexity. 
 	Space Analysis: O(n) -> Depends on the size of the list given.  
 */
